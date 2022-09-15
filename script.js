08a25c49f68819cbbf4c861bdbb16451d13b9469
yevagav <yevagavrilov@gmail.com>
2022-09-14 19:45:55 -0700

    Added quizOne array of objects and start game function

--- script.js
+++ script.js
@@ -25,5 +25,141 @@ submitName.addEventListener('click', () => {
 
 // Player chooses which category they want to challenge 
 
+//Once the category is chosen the game starts 
+const displayQuestion = document.getElementById('questions')
+console.log(displayQuestion)
+
+const displayQuestionNo = document.getElementById('num')
+console.log(displayQuestionNo)
+
+const displayTimer = document.querySelector('#time')
+console.log(displayTimer)
+
+const displayScore = document.querySelector('#total')
+console.log(displayScore)
+
+const displayAnswers = document.querySelectorAll('.answers')
+console.log(displayAnswers)
+
+
+
+const quizOne = [
+    {   
+        questionNumber: 1,
+        questions: 'Which designer logo is seen here?',
+        answers: ['Chanel', 'Givenchy', 'Valentino', 'Gucci'],
+        correctAnswer: 0,
+    },
+    {   
+        questionNumber: 2,
+        questions: 'What brand is associated with this logo?',
+        answers: ['Versace', 'Hermes', 'Louis Vuitton', 'Burberry'],
+        correctAnswer: 1,
+    },
+    {   
+        questionNumber: 3,
+        questions: 'Which logo is seen in the image?',
+        answers: ['Alexander McQueen', 'Prada', 'Armani', 'Givenchy'],
+        correctAnswer: 2,
+    },
+    {   
+        questionNumber: 4,
+        questions: 'The logo corresponds to the brand_______.',
+        answers: ['Baleciaga', 'Pierre Cardin', 'Prada', 'Polo'],
+        correctAnswer: 3,
+    },
+    {   
+        questionNumber: 5,
+        questions: 'The brand seen here is_________.',
+        answers: ['Prada', 'Dior', 'Versace', 'Gucci'],
+        correctAnswer: 2,
+    }
+
+]
+
+const timer = () => {
+    displayTimer.innerText = 10;
+    setInterval(function(){
+
+    })
+}
+
+
+
+const startGame = () => {
+
+        displayQuestionNo.innerText = quizOne[0].questionNumber;
+
+        displayQuestion.innerText = quizOne[0].questions;
+
+        displayAnswers.forEach(function(element, index) {
+            element.textContent = quizOne[0].answers[index];
+
+            element.addEventListener('click', () => {
+                if(quizOne[0].correctAnswer === index) {
+                    displayQuestion.innerText = 'Correct Answer!'
+                } else {
+                    displayQuestion.innerText = 'Wrong Its Chanel'
+                }
+            })
+        
+        })
+
+
+
+
+    }
+
+
+    startGame()
+
+
+
+    // {
+    //     questionNumber: 2,
+    //     questions: 'What brand is associated with this logo?'
+    // }
+    // {
+    //     questionNumber: 3,
+    //     questions: 'Which logo is seen in the image?'
+    // }
+    // {
+    //     questionNumber: 4,
+    //     questions: 'The logo corresponds to the brand_______.'
+    // },
+    // {
+    //     questionNumber: 5,
+    //     questions: 'Which of the following brands is seen here?'
+    // },
+    // {
+    //     questionNumber: 6,
+    //     questions:'Which of these designer brand logos is seen in the picture?'
+
+    // },
+    // {
+    //     questionNumber: 7,
+    //     questions:'The brand seen here is______.'
+    // },
+    // {
+    //     questionNumber: 8,
+    //     questions: 'The logo shown is of the brand____.'
+    // },
+    // {
+    //     questionNumber: 9,
+    //     questions: 'The brand represented in the image is______.'
+
+    // },
+    // {
+    //     questionNumber: 10,
+    //     questions: 'What brand logo is shown here?'
+    // }
+       
+
+
+
+
+
+
+
 
 
